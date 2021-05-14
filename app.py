71dda7dc88f9b8b8179d19fda307409327135ba0
Dan <38283627+trojaxat@users.noreply.github.com>
2021-05-14 17:41:58 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+
+data = pd.read_csv("spiced_projects\\project1\\avocado.csv", sep=',')
+
+data = data.query("type == 'conventional' and region == 'Albany'")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    children=[
+        html.H1(children="Avocado Analytics",),
+        html.P(
+            children="Analyze the behavior of avocado prices"
+            " and the number of avocados sold in the US"
+            " between 2015 and 2018",
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+server = app.server
