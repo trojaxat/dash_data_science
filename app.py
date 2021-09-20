1827cf71b53148c79542e544d1bc4c54c864ac04
trojaxat <dan.axford89@gmail.com>
2021-09-21 00:06:39 +0200

    Added heatmaps for the various models

--- app.py
+++ app.py
@@ -7,23 +7,26 @@ import pandas as pd
 import os
 from plotly.subplots import make_subplots
 import dash_html_components as html
-import base64
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
-# from sklearn import svm, datasets
-# from sklearn import metrics
-# from sklearn.metrics import precision_recall_curve
-# from sklearn.metrics import plot_precision_recall_curve
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.svm import SVC
-# from sklearn.metrics import average_precision_score
+import seaborn as sns
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm, datasets
+from sklearn import metrics
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import plot_precision_recall_curve
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import average_precision_score
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 muschelnPath = os.path.join(CURR_DIR, "muscheln.csv")
 
 df = pd.read_csv(muschelnPath, sep=';')
+
+# Clean data
 df = df.drop(columns=["Unnamed: 0", "nr.", "Unnamed: 13"], axis=1)
-df.isna().sum()
 df["extinktion_kontrolle"] = [
     x.replace(',', '.') for x in df["extinktion_kontrolle"]]
 df["extinktion_1"] = [x.replace(',', '.') for x in df["extinktion_1"]]
@@ -33,84 +36,114 @@ df["extinktion_differenz"] = [
 df["extinktion_korrigiert"] = [
     x.replace(',', '.') for x in df["extinktion_korrigiert"]]
 
-# fig = make_subplots(rows=1, cols=2)
-# fig.add_trace(
-#     px.scatter(df, x="volumen", y="parasiten_gesamt", color="zone"),
-#     row=1, col=1
-# )
+# Organise features
+y = df['zone']
+X = df[["extinktion_korrigiert", "volumen",
+        "m.intestinalis", "m.orientalis", "parasiten_gesamt"]]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# fig.add_trace(
-#     px.histogram(df, x="volumen", y="parasiten_gesamt",
-#                        color="zone", marginal="box", hover_data=df.columns),
-#     row=1, col=2
-# )
+# Logisitic regression
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+clf_report = metrics.classification_report(
+    y_test,
+    y_pred,
+    output_dict=True
+)
+clf_df = pd.DataFrame(
+    clf_report).iloc[:-1, :].T
+figLogisticHeatmap = px.imshow(
+    clf_df,
+    labels=dict(x="Heatmap for Logistic Regression", y="", color="Scale"),
+    x=clf_df.columns,
+    y=clf_df.index
+)
+figLogisticHeatmap.update_xaxes(side="top")
 
-# fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
+# Random forest
+rf_model = RandomForestClassifier()
+rf_model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+clf_report = metrics.classification_report(
+    y_test,
+    y_pred,
+    output_dict=True
+)
+clf_df = pd.DataFrame(
+    clf_report).iloc[:-1, :].T
+figForestHeatmap = px.imshow(
+    clf_df,
+    labels=dict(x="Heatmap for Random Forest", y="", color="Scale"),
+    x=clf_df.columns,
+    y=clf_df.index
+)
+figForestHeatmap.update_xaxes(side="top")
+
+# C-Support Vector Classification
+reg_svc = SVC()
+reg_svc.fit(X_train, y_train)
+y_pred = reg_svc.predict(X_test)
+clf_report = metrics.classification_report(
+    y_test,
+    y_pred,
+    output_dict=True
+)
+clf_df = pd.DataFrame(
+    clf_report).iloc[:-1, :].T
+figSvcHeatmap = px.imshow(
+    clf_df,
+    labels=dict(x="Heatmap for Support Vector Classification",
+                y="", color="Scale"),
+    x=clf_df.columns,
+    y=clf_df.index
+)
+figSvcHeatmap.update_xaxes(side="top")
+
 
 figMain = px.scatter(df, x="volumen", y="parasiten_gesamt", color="zone")
 figDist = px.histogram(df, x="volumen", y="parasiten_gesamt",
                        color="zone", marginal="box", hover_data=df.columns)
 
-image_filename = 'plots\scatter.png'  # replace with your own image
-# encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+# image_filename = 'plots\scatter.png'  # replace with your own image
+# app.layout = html.Div(html.Img(src=app.get_asset_url(image_filename)))
 
 app = dash.Dash(__name__)
-app.layout = html.Div(html.Img(src=app.get_asset_url(image_filename)))
-app.layout = html.Div(
-    children=[
-        html.H1(children="Mussels on Sylt",),
-        html.P(
-            children="Analysis of the amount of parasites in mussels dependant on their relative positional space "
-            "and to see if one can predict where the mussel was picked from based on various features",
-        ),
-        # dcc.Graph(
-        #     figure=fig
-        # ),
+app.layout = html.Div([
+    html.H1(children="Mussels on Sylt",),
+    html.P(
+        children="Analysis of the amount of parasites in mussels dependant on their relative positional space "
+        "and to see if one can predict where the mussel was picked from based on various features",
+    ),
+    dcc.Graph(
+        figure=figMain,
+        style={
+            'height': 'auto',
+            'whiteSpace': 'normal'
+        }
+    ),
+    dcc.Graph(
+        figure=figDist
+    ),
+    html.Div(
         dcc.Graph(
-            figure=figMain,
-            style={
-                'height': 'auto',
-                # all three widths are needed
-                # 'minWidth': '500px', 'width': '500px', 'maxWidth': '500px',
-                'whiteSpace': 'normal'
-            }
+            figure=figLogisticHeatmap
         ),
+        style={'width': '33%', 'display': 'inline-block'}
+    ),
+    html.Div(
         dcc.Graph(
-            figure=figDist
+            figure=figForestHeatmap
         ),
-    ]
-)
-# layout = go.Layout(xaxis=dict(domain=[0.0, 0.45]),
-#                    xaxis2=dict(domain=[0.55, 1.0]),
-#                    yaxis2=dict(overlaying='y',
-#                                anchor='free',
-#                                position=0.55
-#                                )
-
-#                    )
-# fig = go.Figure(data=df, layout=layout)
-# app.layout = html.Div(
-#     children=[
-#         html.H1(children="Avocado Analytics",),
-#         html.P(
-#             children="Analyze the behavior of avocado prices"
-#             " and the number of avocados sold in the US"
-#             " between 2015 and 2018",
-#         ),
-#         dcc.Graph(
-#             figure={
-#                 "data": [
-#                     {
-#                         "x": data["Date"],
-#                         "y": data["AveragePrice"],
-#                         "type": "lines",
-#                     },
-#                 ],
-#                 "layout": {"title": "Average Price of Avocados"},
-#             },
-#         ),
-#     ]
-# )
+        style={'width': '33%', 'display': 'inline-block'}
+    ),
+    html.Div(
+        dcc.Graph(
+            figure=figSvcHeatmap
+        ),
+        style={'width': '33%', 'display': 'inline-block'}
+    )
+])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
